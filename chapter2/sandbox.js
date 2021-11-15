f830ef29088c0272112027d81d6faa9804e3044c
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-15 17:54:13 +0100

    js2

---
+++ chapter2/sandbox.js
@@ -0,0 +1,15 @@
+//strings
+console.log('string jest dedykowany typowi tekstowemu');
+
+let email = 'paula.chrustowska@gmail.com';
+console.log(email);
+
+
+//string concatenation
+let firstName = 'Paula';
+let secondName = 'Janina';
+let lastName = 'Chrustowska';
+
+let fullName = firstName + ' ' + secondName + ' ' + lastName;
+console.log(fullName);
+

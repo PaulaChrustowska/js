5fccb8ab3232b83ed2b93824021728a5f5a03647
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-16 19:45:29 +0100

    syntax basic & types end

---
+++ chapter3/boolean.js
@@ -0,0 +1,33 @@
+//booleans & comparisons /typy logiczne i porównania
+
+//console.log(true, false, "true", "false");
+
+
+//METHODS CAN RETURN BOOLEANS
+//let email = 'abrakadabra@ulala.com';
+//let names = ['abrakadabra', 'czarymary', 'hokuspokus'];
+
+
+//let result = email.includes('@');
+
+//let result = email.includes('x');
+
+//let result = email.includes('czarymary');
+//console.log(result);
+
+
+//COMPARISON OPERATORS
+
+let age = 18
+
+
+console.log(age == 18);
+console.log(age == 40);
+console.log(age != 40);
+console.log(age > 40);
+console.log(age < 40);
+
+
+
+
+
---
+++ chapter3/objects.js
@@ -0,0 +1,42 @@
+
+let sweets = ['chocolate', 'cake', 'almondsInChocolate'];
+
+//sweets[1] = 'white';
+//console.log(sweets[1]);
+
+
+//let calories = [200, 250, 300];
+//console.log(calories[2]);
+
+
+//let random = ['flower', 'sky', 24, 12];
+//console.log(random);
+
+
+//console.log(sweets.length);
+
+
+
+//ARRAY METHODS
+
+
+//let result = sweets.join(',');
+
+
+//let result = sweets.indexOf('cake')
+
+
+//let result = sweets.concat(['flower, sky']);
+
+
+ 
+console.log(result);
+
+
+
+
+
+
+ 
+
+

ddb1f5c79f0c12cefc782d0a97599dcc30e6ba9f
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-16 11:27:30 +0100

    kolejny piekny dzień z js

---
+++ chapter3/sandbox.js
@@ -0,0 +1,28 @@
+//common string methods
+
+let email = 'paula.chrustowska@gmail.com';
+
+ //let result = email.lastIndexOf('a');
+
+ // slice method   
+ //rozcina na kawałki zmienną, separuje dany fragment zeminnej/ dwie zmienne
+
+ //let result = email.slice(2,5);
+
+//console.log(result);
+
+// Substr //substring  wycina fragment o konkretnej liczbie znaków zmiennej od wskazanej pozycji
+// np.(4,10) wycina frag od 4 znaku o długosci 10 znaków
+
+
+//let result = email.substr(4,10);
+
+let result = email.replace('p', 'f');
+
+console.log(result);
+
+
+
+
+
+

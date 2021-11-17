702094becf23cc00335e55a0697d117378270781
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-17 16:48:08 +0100

    forLoop

--- chapter4/forLoops.js
+++ chapter4/forLoops.js
@@ -1,37 +1,48 @@
-for(let i = 0; i < 5; i++){
-   console.log('in loop:', i);
+//for(let i = 0; i < 5; i++){
+  // console.log('in loop:', i);
+//}
+
+//console.log('loop finished');
+
+
+
+
+//for(let i = 0; i < 10; i++){
+    //console.log('in loop;', i);
+//}
+
+//console.log('loop finished');
+
+
+
+
+
+//for(let i = 0; i <212; i++){
+   // console.log('in loop:', i);
+//}
+
+//console.log('loop finished');
+
+
+
+
+
+//for(let = 1; i = 12; i++){
+    //console.log('im loop;', i);
+//}
+
+//console.log('loop finished');
+
+
+const fruits = ['jabłko', 'gruszka', 'śliwka'];
+
+for(let i = 0; i < fruits.length; i++){
+    //console.log(fruits[i]);
+
+    let html = `<div>${fruits[i]}</div>`;
+    console.log(html);
+    
+
 }
 
-console.log('loop finished');
 
-
-
-
-for(let i = 0; i < 10; i++){
-    console.log('in loop;', i);
-}
-
-console.log('loop finished');
-
-
-
-
-
-for(let i = 0; i <212; i++){
-    console.log('in loop:', i);
-}
-
-console.log('loop finished');
-
-
-
-
-
-for(let = 1; i = 12; i++){
-    console.log('im loop;', i);
-}
-
-console.log('loop finished');
-
-
-for

2cd64fbcb0025e0163cc99c379f96c40e1a71edb
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-17 15:18:20 +0100

    another day in paradaaaisee

---
+++ chapter3/typeConversion.js
@@ -0,0 +1,21 @@
+//type conversion
+
+//let score = '100';
+
+//score = Number(score);
+
+//console.log(score + 1);
+//console.log(typeof score);
+
+
+//let score = '2020';
+
+//score = Number(score);
+//console.log(score + 25);
+//console.log(typeof score);
+
+
+
+
+
+
---
+++ chapter4/forLoops.js
@@ -0,0 +1,35 @@
+for(let i = 0; i < 5; i++){
+   console.log('in loop:', i);
+}
+
+console.log('loop finished');
+
+
+
+
+for(let i = 0; i < 10; i++){
+    console.log('in loop;', i);
+}
+
+console.log('loop finished');
+
+
+
+
+
+for(let i = 0; i <212; i++){
+    console.log('in loop:', i);
+}
+
+console.log('loop finished');
+
+
+
+
+
+for(let = 1; i = 12; i++){
+    console.log('im loop;', i);
+}
+
+console.log('loop finished');
+

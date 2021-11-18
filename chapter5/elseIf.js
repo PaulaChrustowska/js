c2bf299e35e1d1dbf0a3323f63abb6846b47888c
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-18 17:54:02 +0100

    elseIf

---
+++ chapter5/elseIf.js
@@ -0,0 +1,11 @@
+const password = 'akukaracza';
+
+if(password.length >= 8){
+    console.log("it's correct!");
+
+}  else if(password.length >= 20){
+    console.log("password is Ok!");
+    
+} else {
+    console.log('password is not long enough');
+}

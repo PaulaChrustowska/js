69948ab73fae7b4f13e2426dce243ff699681e3c
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-18 15:35:35 +0100

    if

--- chapter5/if.js
+++ chapter5/if.js
@@ -1,16 +1,27 @@
 //if statement
 
-const age = 40;
+//const age = 40;
 
-if(age > 35){
-    console.log('you must have your health checkups');
+//if(age > 35){
+   // console.log('you must have your health checkups');
+//}
+
+//if(age > 30){
+//    console.log('you must eat more vegetables');
+//}
+
+//if(age > 20){
+  //  console.log('you must sleep more');
+//}
+
+//const fruits = ['apple', 'orange', 'banana', "plum"];
+
+//if(fruits.length > 3){
+  //  console.log("that's a lot of fruits")
+//}
+
+const password = 'blue';
+
+if(password.length >= 4){
+    console.log("it's OK");
 }
-
-if(age > 30){
-    console.log('you must eat more vegetables');
-}
-
-if(age > 20){
-    console.log('you must sleep more');
-}
-

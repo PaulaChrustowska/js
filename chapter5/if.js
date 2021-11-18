b10801c78b3473bc2921f45c8dbfd192d6852be0
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-18 14:50:23 +0100

    wprowadzenie if

---
+++ chapter5/if.js
@@ -0,0 +1,16 @@
+//if statement
+
+const age = 40;
+
+if(age > 35){
+    console.log('you must have your health checkups');
+}
+
+if(age > 30){
+    console.log('you must eat more vegetables');
+}
+
+if(age > 20){
+    console.log('you must sleep more');
+}
+

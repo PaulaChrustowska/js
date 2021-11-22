bfb9ef7e75a6f51c832a076762d7d136f2408a16
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-22 18:15:31 +0100

    returning values

---
+++ chapter7/returningValues.js
@@ -0,0 +1,37 @@
+// returning values
+
+// function multiply(a = 12, b = a / 2, c = a * b){
+//     console.log(a, b, c);
+//     return a * b * c;
+
+// };
+//  multiply();
+
+
+// const calcArea = function(pole){
+//      let area = 6 * pole;
+//      console.log(area);
+
+//  }
+//  //calcArea(5);
+//  calcArea(23.4);
+
+
+
+//FUNKCJA STRZAŁKOWA / ARROW FUNCTION
+
+
+// const greet = function(){
+//    console.log('have a good monday!');
+//     }
+    
+//      greet();
+
+
+     const greet = () => {
+        console.log('have a good monday!');
+         }
+         
+          greet();
+
+

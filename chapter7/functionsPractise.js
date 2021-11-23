1d4f89fbc4a09dde5e8f4067a0db2fd6b3199f7f
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-23 21:45:09 +0100

    functionsPractise

---
+++ chapter7/functionsPractise.js
@@ -0,0 +1,15 @@
+// const hi = () => {
+//     console.log("cześć!");
+// }
+// hi();
+
+
+const sayHello = (name, surname) => {
+    console.log("Hello" + " " + "beautiful" + " " + name + " " + surname);
+    
+ 
+}
+sayHello("kasia", "bąk");
+sayHello('witek', "kwitek");
+
+

3c0ede29827fcf40b843faf47eb24528b6e9563c
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-23 20:45:49 +0100

    arrowPractise

--- chapter7/practiseArrow.js
+++ chapter7/practiseArrow.js
@@ -17,6 +17,33 @@
 
 // console.log(bill([5 ,15,30], 0.2));
 
-const numbers = [2, 5, 7, 10, 34, 16, 879, 1]
-const parzyste = () => numbers.filter n => n % 2;
-console.log(parzyste);
+
+
+
+// const numbers = [2, 5, 7, 10, 34, 16, 879, 1]
+// const parzyste = () => numbers.filter(n => n % 2 ===0 );
+// console.log(parzyste());
+
+
+
+
+const numbers = [52, 85, 69, 985, 45, 66, 86, 12, 24, 84]
+// const parzyste = () => numbers.filter( n => n % 4 ===0); 
+// console.log(parzyste());
+
+
+// const liczba = () => numbers.filter( n => n > 52 );
+// console.log(liczba());
+
+
+// const podzielne5 = () => numbers.filter( n => n % 5 ===0);
+// console.log(podzielne5());
+
+
+// const podzielne3 = () => numbers.filter( n => n % 3 ===0);
+// console.log(podzielne3());
+
+const rózne = () => numbers.filter( n => n != 52);
+console.log(rózne());
+
+

e26dc210291e7147e4fab95d56cd944fb70a8a19
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-24 12:29:02 +0100

    filter

---
+++ chapter7/filter.js
@@ -0,0 +1,43 @@
+// cw 1
+
+
+// const carbsFruits_oneCup = [11, 13, 14, 15, 21, 22, 25]
+// const fruit = f => f >= 13
+// const result = carbsFruits_oneCup.filter(fruit)
+// console.log(result)
+
+//cw 2
+//rozmiary mniejsze równe 37
+
+const numberOfShoes = [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46]
+// const shoes = s => s <= 37
+// const result = numberOfShoes.filter(shoes)
+// console.log(result)
+
+
+//rozmiary  większe od 40
+
+// const shoes = s => s > 40
+// const result = numberOfShoes.filter(shoes)
+// console.log(result)
+
+
+//rozmiary posortowane w kolejnosci od największego do najmniejszego
+// const result = numberOfShoes.sort(function(a,b){return b - a});
+// console.log(result)
+
+
+const result = numberOfShoes.sort(function{return b - a});
+
+
+
+//cw 3
+
+// const numbersOf_students = [12, 14, 52, 10, 87, 56, 45, 89, 11, 10, 56]
+// // const result = numbersOf_students.sort()
+// // console.log(result)
+
+// const result = numbersOf_students.reverse()
+// console.log(result)
+
+

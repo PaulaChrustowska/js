205f80409e5d507d4ab5b81a04bb8a6f3b3c4a0f
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-26 10:53:13 +0100

    map

--- chapter7/map.js
+++ chapter7/map.js
@@ -6,6 +6,7 @@ const workers = [
       department: "Director",
       sex: 'male',
       fav_fruit: "apple",
+      hair_color: 'red',
     },
     {
       id: 13,
@@ -14,6 +15,8 @@ const workers = [
       department: "Administration",
       sex: 'male',
       fav_fruit: "tomato",
+      hair_color: 'blond',
+
 
     },
     {
@@ -23,6 +26,8 @@ const workers = [
       department: "Manager",
       sex: 'female',
       fav_fruit: "banana",
+      hair_color: 'grey',
+
     },
     {
       id: 99,
@@ -30,15 +35,31 @@ const workers = [
       age: 41,
       department: "HR specialist",
       sex: 'male',
-      fav_fruit: 'raspberry'
+      fav_fruit: 'raspberry',
+      hair_color: 'black',
     }]
 
-    const age = a => a.age >=  19
-    const department = d => d.department === "Director"
-    const result = workers
-                        .filter(age)
-                        .filter(department)
-                        .map(g => `Hi, my favourite fruits is: ${g.fav_fruit} and my age is: ${g.age}`)
+    // const age = a => a.age != 19
+    // const department = d => d.department === "Director"
+    // const result = workers
+    //                     .filter(age)
+    //                     .filter(department)
+    //                     .map(g => `Hi, my favourite fruits is: ${g.fav_fruit} and my age is: ${g.age}`)
 
+
+    // const id = i => i.id > 9
+    // const age = a => a.age != 19
+    // const sex = s => s.sex = 'male' && 'female'
+    // const result = workers
+    //                         .filter(id)
+    //                         .map( s => `Hi, my sex is: ${s.sex} and my age is: ${s.age}`)
+
+    const id = i => i.id > 13
+    const age = a => a.age = 41
+    const sex = s => s.sex = 'male' && 'female'
+    const result = workers 
+                
+                            .filter(age)
+                            .map( s => `Hello, my age is: ${s.age} and my hair_color is: ${s.hair_color}`)
     console.log(result)
                      

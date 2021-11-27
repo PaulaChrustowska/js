0eeedb8374174091ed7d9414d1d59e276469da76
Krzysztof Tucholski <k.tucholski@gmail.com>
2021-11-27 20:57:13 +0100

    Krises commit

--- chapter7/callback.js
+++ chapter7/callback.js
@@ -1,10 +1,10 @@
 // const multiply = cyfra => cyfra * 3
 
 // const arr = [3,5,8].map(multiply)
-    
+
 //     console.log(arr)
 
- 
+
 
 // const multiply = (x) => x * 2
 
@@ -23,6 +23,62 @@
 // const result = arr.map(add)
 
 // console.log(result)
+
+const users = [
+    {
+        name: 'Krzys',
+        age: 35,
+        sex: 'male',
+        interests: [
+            {
+                title: 'bushcraft',
+                level: 'hard',
+                experienceInYears: 2
+            },
+            {
+                title: 'cycling',
+                level: 'expert',
+                experienceInYears: 20
+            }
+        ],
+    },
+    {
+        name: 'Hubert',
+        age: 5,
+        sex: 'male',
+        interests: [
+            {
+                title: 'lego',
+                level: 'entry',
+                experienceInYears: 2
+            },
+            {
+                title: 'fauna',
+                level: 'intermediate',
+                experienceInYears: 2
+            },
+            {
+                title: 'flora',
+                level: 'entry',
+                experienceInYears: 2
+            }
+        ],
+    },
+    {
+        name: 'Emilka',
+        age: 2,
+        sex: 'female',
+        interests: [
+            {
+                title: 'lego',
+                level: 'entry',
+                experienceInYears: 2
+            }
+        ],
+    }
+]
+
+
 
 
 

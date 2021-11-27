083acb8e185580c936a0706fbc5c09c359a10668
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-27 11:44:49 +0100

    callback

---
+++ chapter7/callback.js
@@ -0,0 +1,35 @@
+// const multiply = cyfra => cyfra * 3
+
+// const arr = [3,5,8].map(multiply)
+    
+//     console.log(arr)
+
+ 
+
+// const multiply = (x) => x * 2
+
+// const arr = [3, 8, 19, 21, 3, 6]
+
+// const result = arr.map(multiply)
+
+// console.log(result)
+
+
+
+// const add = (y) => y + 5
+
+// const arr = [5,8,9,41,78,10]
+
+// const result = arr.map(add)
+
+// console.log(result)
+
+
+
+const add = (y, x) => y + x
+
+const arr = [5, 8]
+const result = arr.map(add)
+
+console.log(result)
+

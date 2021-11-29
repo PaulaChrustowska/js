40873e0dff785502b8f93864820fdb61ae1746e8
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-29 17:36:56 +0100

    exercises

--- chapter7/cb.js
+++ chapter7/cb.js
@@ -1,11 +1,51 @@
-const szafa: {
-    kolor: 'szary',
-    status: 'zapelniona',
-    typObciazenia: [
-        {
-            ksiazki: [
-                
-            ]
-    
 
-}
+
+
+const room = [
+    {
+        furniture: 'warderobe',
+        colour: 'grey',
+        status: 'full',
+        books: [
+             {
+                type: 'adventure',
+                old: false,
+                size: 3
+             },
+             { 
+    
+                type: 'comics',
+                new: true,
+                size: 5
+            }
+        ],
+    },
+    {
+        furniture: 'rack',
+        colour: 'white',
+        status: 'almostEmpty',
+        ssweaters: [
+            {
+                type: 'woll',
+                new: true,
+                size: 42
+            }
+        ],
+
+    },
+    {
+        furniture: 'dresser',
+        kolor: 'brown',
+        status: 'almostEmpty',
+        mugs: [
+            {
+                type: 'porcelain',
+                new: true,
+                size: 6
+            }
+        ],  
+    }
+]
+
+
+        

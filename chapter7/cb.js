0c0772187ba9525414b17a1a54367802c6d4b968
PaulaChrustowska <paula.chrustowska@interia.pl>
2021-11-30 16:54:11 +0100

    in a room

--- chapter7/cb.js
+++ chapter7/cb.js
@@ -72,5 +72,25 @@ const room = [
 // const hasTwoShelfs = thing => thing.shelf.length ===  2
 // console.log(hasTwoShelfs(room[0]))
 
-const hasOneShelfs = thing => thing.shelf.length === 1 
- console.log(hasOneShelfs(room[1]))
+// const hasOneShelfs = thing => thing.shelf.length === 1 
+//  console.log(hasOneShelfs(room[1]))
+
+
+// const whatSize = thing => thing.shelf[0].size === 6
+// const isDresser = thing => thing.furniture === 'dresser'
+// const result = room.filter(whatSize).filter(isDresser)
+// console.log(result)
+
+
+// const whatStatus = thing => thing.status === 'almostEmpty'
+// const descriptionOf = thing => `hello, my name is: ${thing.furniture}`
+// const isRack = thing => thing.furniture === 'rack'
+// const result = room.filter(whatStatus).filter(isRack).map(descriptionOf)
+
+
+
+const isFull = thing => thing.status === 'full'
+// const descriptionOf = thing => `hello, my name is: ${thing.furniture}`
+const isWarderobe = thing => thing.furniture === 'warderobe'
+const result = room.filter(isFull).filter(isWarderobe)
+console.log(result)
